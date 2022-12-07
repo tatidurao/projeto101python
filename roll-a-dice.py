42161d05522c086788c65dda1f5023f4e694eb2b
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2022-12-07 13:43:46 -0300

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,29 @@
+import random 
+  
+# Use uma variável para armazenar a resposta para continuar jogando o dado
+
+#crie um while  
+
+      
+    # Gere um número aleatório 
+    # entre 1 e 6 (incluindo 
+    # os números 1 e 6) 
+    no = random.randint(1,6) 
+
+    # condições para verificar o valor numérico  
+    if no == 1: 
+        print("[-----]") 
+        print("[     ]") 
+        print("[  0  ]") 
+        print("[     ]") 
+        print("[-----]") 
+    if no == 2: 
+        print("[-----]") 
+        print("[ 0   ]") 
+        print("[     ]") 
+        print("[   0 ]") 
+        print("[-----]") 
+    #crie os proximos if 
+          
+    # Peça para o usuário informar uma resposta      
+    
